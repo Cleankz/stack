21af1acc31b6d0aa04dc1b74e2177cfef2cd246c
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-04-09 09:57:58 +0300

    Create stac.py

---
+++ stac.py
@@ -0,0 +1,43 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        if len(self.stack) != 0:
+            a = self.stack[0]
+            del self.stack[0]
+            return a
+        return None # если стек пустой
+
+    def push(self, value):
+        self.stack.insert(len(self.stack),value)
+        # ваш код
+
+    def peek(self):
+        if len(self.stack) != 0:
+            return self.stack[0]
+        return None # если стек пустой
+
+def balans(string):
+    stack = Stack()
+    for i in string:
+        stack.push(i)
+    sum_open = 0
+    sum_close = 0
+    while stack.size() > 0:
+        l = stack.size
+        if stack.peek() == ")":
+            return False
+        if stack.pop() =="(" and stack.size() == 1:
+            return False
+        if stack.pop() =="(":
+            sum_open +=1
+        else:
+            sum_close +=1
+    if sum_open == sum_close:
+        return True
+    else:
+        return False
